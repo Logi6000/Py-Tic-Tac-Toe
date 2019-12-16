a22e4b5be79c9e9feed5deed941e6777a7d70e0e
Logan Purcell <lgnpurcell1@gmail.com>
2019-12-16 15:43:51 -0500

    Just Little changes

--- main.py
+++ main.py
@@ -4,26 +4,42 @@ import os
 
 DISPLAY_WIDTH = 640
 DISPLAY_HEIGHT = 640
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 pygame.init()
 
-game_window = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+gameWindow = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
 
 pygame.display.set_caption("Tic Tac Toe")
 
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameWindow.fill(WHITE)
+
 def game_loop():
+    ## Game Content Here
+        
+    ## Quit Function Here
     game_running = True
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
 
-            print(event)
-
         pygame.display.update()
         clock.tick(60)
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
