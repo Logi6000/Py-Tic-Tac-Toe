0ea22353c34940fdf075d3cb5e84073c8994776c
Logan Purcell <lgnpurcell1@gmail.com>
2019-12-16 15:34:21 -0500

    Add a simple window

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import pygame
 import time
 import os
 
+DISPLAY_WIDTH = 640
+DISPLAY_HEIGHT = 640
 
 pygame.init()
 
+game_window = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Tic Tac Toe")
+
+def game_loop():
+    game_running = True
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+
+            print(event)
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
+pygame.quit()
+quit()
